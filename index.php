ab8043a4440d494edb70ae28cfff84621a441def
Dan <33081566+ddmrz@users.noreply.github.com>
2018-11-27 18:23:36 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
+<?php
+	include("mysql.php");
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Music DB</title>
 </head>
 <body>
+	<h1>Music DB</h1>
 <?php
-	print "Hello World!\n"
+	$sql = $dbc->query('SELECT * FROM Song WHERE 1');
+	while ( $song = $sql->fetch_assoc() ) {
+    	echo "{$song['artist']} - {$song['title']} - {$song['album']} - {$song['releaseYear']} <br>";
+	}
+	$sql->free();
+	$dbc->close();
 ?>
 </body>
 </html>
