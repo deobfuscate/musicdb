32f1482d76e1c30df0458d52bbfef38a186bed61
sk8moola <45409288+sk8moola@users.noreply.github.com>
2018-11-28 17:58:53 -0800

    Update SignUp.php
    My code is kind of a mess since I don't know how to connect the data inputted by the user to the database, but the outline is there.

--- SignUp.php
+++ SignUp.php
@@ -1,15 +1,17 @@
 <?php
 include("mysql.php");
-$servername = "localhost";
+/*$servername = "localhost";
 $username = "username";
 $password = "password";
-$dbname = "myDB";      //CHANGE THESE LATER	
+$dbname = "myDB";	
 	
-$conn = new msqli($servername, $username, $password, $dbname); //establishes connection
+$conn = new msqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
+*/
+//above lines except for 2 not needed
 
 $sql = "INSERT INTO users (userID, password, firstName, lastName, genrePref)
 VALUES (username, password, firstname, lastname, favgenre)";
