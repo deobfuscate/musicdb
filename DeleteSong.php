cf3fe3f9d8e687d9035274d6b75ad478e55adf37
sk8moola <45409288+sk8moola@users.noreply.github.com>
2018-11-28 19:42:33 -0800

    Create DeleteSong.php
    The outlines are all there, but a bit of work still needs to be done

---
+++ DeleteSong.php
@@ -0,0 +1,25 @@
+<?php
+include("mysql.php");
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// sql to delete a record
+$sql = "DELETE FROM Song WHERE title = "; //change this to the current song
+
+if ($conn->query($sql) === TRUE) {
+    echo "Record deleted successfully";
+} else {
+    echo "Error deleting record: " . $conn->error;
+}
+
+$conn->close();
+?>
