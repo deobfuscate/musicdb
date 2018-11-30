ff768cbb0210dcff81fdb1748e44c02d58d86139
Dan <33081566+ddmrz@users.noreply.github.com>
2018-11-29 17:26:14 -0800

    Update SignUp.php

--- SignUp.php
+++ SignUp.php
@@ -1,27 +1,23 @@
 <?php
-include("mysql.php");
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";	
-	
-$conn = new msqli($servername, $username, $password, $dbname);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	include("mysql.php");
+	// define variables and set to empty values
+	$userID = $userPassword = $firstName = $lastName = $genrePref = "";
+	$userID = test_input($_POST["username"]);
+	$userPassword = test_input($_POST["password"]);
+	$firstName = test_input($_POST["firstname"]);
+	$lastName = test_input($_POST["lastname"]);
+	$genrePref = test_input($_POST["favgenre"]);
 
-if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
+	$sql = "INSERT INTO User (userID, password, firstName, lastName, genrePref)
+VALUES ($userID, $userPassword, $firstName, $lastName, $genrePref)";
+	if ($dbc->query($sql) === TRUE {
+		echo "New user created successfully<br>\n";
+		echo "<a href="./">Go Back</a>";
+	}
+	else {
+		echo "Error: " . $sql . "<br>" . $dbc->error;
+	}
+	$dbc->close();
 }
-
-//above lines except for 2 not needed
-
-$sql = "INSERT INTO User (userID, password, firstName, lastName, genrePref)
-VALUES (username, password, firstname, lastname, favgenre)";
-
-if ($conn->query($sql) === TRUE {
-	echo "New record created successfully";
-} 
-else {
-	echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
 ?>
