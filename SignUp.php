0812626ce9e1e4df5633c4b2c0b3fb320931ba46
Dan <33081566+ddmrz@users.noreply.github.com>
2018-11-29 17:40:49 -0800

    Update SignUp.php

--- SignUp.php
+++ SignUp.php
@@ -3,11 +3,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	include("mysql.php");
 	// define variables and set to empty values
 	$userID = $userPassword = $firstName = $lastName = $genrePref = "";
-	$userID = test_input($_POST["username"]);
-	$userPassword = test_input($_POST["password"]);
-	$firstName = test_input($_POST["firstname"]);
-	$lastName = test_input($_POST["lastname"]);
-	$genrePref = test_input($_POST["favgenre"]);
+	$userID = parseInput($_POST["username"]);
+	$userPassword = parseInput($_POST["password"]);
+	$firstName = parseInput($_POST["firstname"]);
+	$lastName = parseInput($_POST["lastname"]);
+	$genrePref = parseInput($_POST["favgenre"]);
 
 	$sql = "INSERT INTO User (userID, password, firstName, lastName, genrePref)
 VALUES ({$userID}, {$userPassword}, {$firstName}, {$lastName}, {$genrePref})";
@@ -19,5 +19,11 @@ VALUES ({$userID}, {$userPassword}, {$firstName}, {$lastName}, {$genrePref})";
 		echo "Error: " . $sql . "<br>" . $dbc->error;
 	}
 	$dbc->close();
+}
+function parseInput($data) {
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+ 	return $data;
 }
 ?>
