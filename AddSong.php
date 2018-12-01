77d1141cacc003202c32dc6936defe09734c3289
Cody <codymorgan89@gmail.com>
2018-11-30 22:24:03 -0800

    fixed connection syntax to mysqli

--- AddSong.php
+++ AddSong.php
@@ -5,7 +5,7 @@ $username = "username";
 $password = "password";
 $dbname = "myDB";	
 	
-$conn = new msqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
