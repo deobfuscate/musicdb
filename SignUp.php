5c6dbea996a3e70a57ac064c4bc0541f9a552102
Dan <33081566+ddmrz@users.noreply.github.com>
2018-11-30 17:15:02 -0800

    Update SignUp.php

--- SignUp.php
+++ SignUp.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color:rgb(166, 192, 179);">
+
+<h2>Welcome to MusicDB </h2>
+<h3> Join the largest music database in the world! </h3>
+
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	include("mysql.php");
@@ -13,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 VALUES ({$userID}, {$userPassword}, {$firstName}, {$lastName}, {$genrePref})";
 	if ($dbc->query($sql) === TRUE) {
 		echo "New user created successfully<br>\n";
-		echo "<a href="./">Go Back</a>";
+		echo "<a href=\"./\">Go Back</a>";
 	}
 	else {
 		echo "Error: " . $sql . "<br>" . $dbc->error;
@@ -27,3 +34,5 @@ function parseInput($data) {
  	return $data;
 }
 ?>
+</body>
+</html>
