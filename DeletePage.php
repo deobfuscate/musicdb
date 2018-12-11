1628f62c26ec3f12ea7bf1f3a0f1b28c9db0c781
Dan <33081566+ddmrz@users.noreply.github.com>
2018-12-10 16:38:12 -0800

    Update DeletePage.php

--- DeletePage.php
+++ DeletePage.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html>
-<body>
-<h1> Are you sure you want to delete this song? </h1>
-<h2> Type in your username and password to confirm </h2>
+<body style="background-color:rgb(166, 192, 179);">
+<h1>Are you sure you want to delete this song?</h1>
+<h2>Type in your username and password to confirm</h2>
 <form action="DeleteSong.php" method="POST">
   Username:<br>
   <input type="text" name="username"><br>
   Password:<br>
   <input type="text" name="password"><br><br>
+  <input type="hidden" name="songID" value="<?php echo $_GET["songid"]; ?>">
   <input type="submit" value="Delete">
 </form>
 </body>
